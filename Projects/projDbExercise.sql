debb0bc8a9a03c24bd21d300a8038ee43fda60c4
David Hull <david.hull1@utexas.edu>
2018-04-05 10:21:35 -0500

    Updated ProjectsDBExercises

--- Projects/projDbExercise.sql
+++ Projects/projDbExercise.sql
@@ -1,3 +1,15 @@
 SELECT * FROM project_uses_tech
 JOIN tech
   ON project_uses_tech.tech_id = tech.id;
+
+SELECT * FROM project_uses_tech
+JOIN tech
+  ON project_uses_tech.tech_id = tech.id
+  WHERE project_id=4;
+
+
+SELECT * FROM tech
+LEFT JOIN project_uses_tech
+    ON project_uses_tech.tech_id = tech.id;
+
+SELECT
